78f3c80153cccf89db13dcbf2934fdbe27f1b4bf
Suzanne Li <lisuzanne94@gmail.com>
2021-03-15 21:47:02 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -13,10 +13,10 @@ function teenager(age) {
 function ageChecker(age) {
   if (age >=13 && age <= 19) {
     return "You are a teenager!"
-  } else if (age <= 12) {
-    return "You are a kid"
-  } else if (age >= 20) {
-    return "You are a grownup"
+  }   else if (age <= 12) {
+        return "You are a kid"
+  }   else (age >= 20) {
+        return "You are a grownup"
   }
 }
 
